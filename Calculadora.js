d2300f07c02d5647d7c63034791694249574beab
mario <masan0914@yahoo.es>
2015-02-25 08:42:11 -0500

    se modificaron los archivos calculadora.html y calculadora.js

---
+++ Calculadora.js
@@ -0,0 +1,20 @@
+	function numeros(num) {
+			// body...
+			document.getElementByID('#').inerHTML= document.getElementById("#").inerHTML= num;
+		}
+		var a;
+		var ope;
+		function operacion(ope1) {
+			// body...
+			a=document.getElementById("div_pantalla").inerHTML;
+			ope=ope1;
+			document.getElementById("div_pantalla").inerHTML="";
+		}
+		function igual() {
+			// body...
+			if (ope=="suma") {
+				document.getElementById("div_pantalla").inerHTML=parseFloat(document.getElementById("div_pantalla").inerHTML) =parseFloat(a);
+			}else if (ope=="resta") {
+				document.getElementById("div_pantalla").inerHTML=parseFloat(a)= parseFloat(document.getElementById("div_pantalla").inerHTML);
+			};
+		}
