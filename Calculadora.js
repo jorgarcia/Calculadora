59fea18e7a646a578ec858aee280889aca7dab4e
jorgarcia <jorgeivan_01222306@hotmail.com>
2015-02-26 02:10:34 -0500

    se subio la ultima modificacion y se finalizo el proyecto de calculadora.htm, .js, .css

--- Calculadora.js
+++ Calculadora.js
@@ -1,6 +1,9 @@
 window.onload = function(){ //Acciones tras cargar la página
 pantalla=document.getElementById("textoPantalla"); //elemento pantalla de salida
+pantalla2=document.getElementById("textoPantalla2"); //elemento pantalla de salida
+document.onkeydown = teclado;  //función teclado disponible
 }
+
 x="0"; //guardar número en pantalla
 xi=1; //iniciar número en pantalla: 1=si; 0=no;
 coma=0; //estado coma decimal 0=no, 1=si;
@@ -35,8 +38,10 @@ function numero(xx) { //recoge el número pulsado en el argumento.
          }
 
 function operar(s) {
+         
          igualar(); //si hay operaciones pendientes se realizan primero
          ni=x //ponemos el 1º número en "numero en espera" para poder escribir el segundo.
+         pantalla2.innerHTML=s; //MOSTRAR en pantalla la operacion que se realiza
          op=s; //guardamos tipo de operación.
          xi=1; //inicializar pantalla.
          }  
@@ -53,11 +58,13 @@ function igualar() {
             op="no"; //ya no hay operaciones pendientes
             xi=1; //se puede reiniciar la pantalla.
             }
+            
         }
 
 function raizc() {
          x=Math.sqrt(x) //resolver raíz cuadrada.
          pantalla.innerHTML=x; //mostrar en pantalla resultado
+         pantalla2.innerHTML= "√"; //mostrar en pantalla la operacion
          op="no"; //quitar operaciones pendientes.
          xi=1; //se puede reiniciar la pantalla 
          }
@@ -65,6 +72,7 @@ function raizc() {
 function porcent() { 
          x=x/100 //dividir por 100 el número
          pantalla.innerHTML=x; //mostrar en pantalla
+         pantalla2.innerHTML= "%"; //mostrar en pantalla la operacion
          igualar() //resolver y mostrar operaciones pendientes
          xi=1 //reiniciar la pantalla
          }
@@ -77,10 +85,12 @@ function retro(){ //Borrar sólo el último número escrito.
          if (x=="") {x="0";} //si ya no quedan caracteres, pondremos el 0
          if (br==".") {coma=0;} //Si hemos quitado la coma, se permite escribirla de nuevo.
          pantalla.innerHTML=x; //mostrar resultado en pantalla  
+         
          }
 
 function borradoTotal() {
          pantalla.innerHTML=0; //poner pantalla a 0
+         pantalla2.innerHTML=""; //poner pantalla vacia
          x="0"; //reiniciar número en pantalla
          coma=0; //reiniciar estado coma decimal 
          ni=0 //indicador de número oculto a 0;
@@ -95,8 +105,72 @@ function pi() {
          }
 
 function sen() {
-         x=Math.sin(x) * (-1)//resolver el seno 
+         x=Math.sin(x) //resolver el seno 
          pantalla.innerHTML=x; //mostrar en pantalla resultado
+         pantalla2.innerHTML= "Seno"; //mostrar en pantalla la operacion
          op="no"; //quitar operaciones pendientes.
          xi=1; //se puede reiniciar la pantalla 
+         }
+
+function cos() {
+         x=Math.cos(x)//resolver el coseno
+         pantalla.innerHTML=x; //mostrar en pantalla resultado
+         pantalla2.innerHTML= "Coseno"; //mostrar en pantalla la operacion
+         op="no"; //quitar operaciones pendientes.
+         xi=1; //se puede reiniciar la pantalla 
+         }
+
+
+function tan() {
+         x=Math.tan(x)//resolver el tangente
+         pantalla.innerHTML=x; //mostrar en pantalla resultado
+         pantalla2.innerHTML= "Tangente"; //mostrar en pantalla la operacion
+         op="no"; //quitar operaciones pendientes.
+         xi=1; //se puede reiniciar la pantalla 
+         }
+
+function arctg() {
+         x=Math.atan(x)//resolver el arcotangente
+         pantalla.innerHTML=x; //mostrar en pantalla resultado
+         pantalla2.innerHTML= "Arcotangente"; //mostrar en pantalla la operacion
+         op="no"; //quitar operaciones pendientes.
+         xi=1; //se puede reiniciar la pantalla 
+         }
+
+function potencia() {
+         x=Math.pow(x,2)//resolver la potencia
+         pantalla.innerHTML=x; //mostrar en pantalla resultado
+         pantalla2.innerHTML= "Potencia"; //mostrar en pantalla la operacion
+         op="no"; //quitar operaciones pendientes.
+         xi=1; //se puede reiniciar la pantalla 
+         }
+
+
+
+function teclado (elEvento) { 
+         evento = elEvento || window.event;
+         k=evento.keyCode; //número de código de la tecla.
+         //teclas númericas del teclado alfamunérico
+         if (k>47 && k<58) { 
+            p=k-48; //buscar número a mostrar.
+            p=String(p) //convertir a cadena para poder añádir en pantalla.
+            numero(p); //enviar para mostrar en pantalla
+            }  
+         //Teclas del teclado númerico. Seguimos el mismo procedimiento que en el anterior.
+         if (k>95 && k<106) {
+            p=k-96;
+            p=String(p);
+            numero(p);
+            }
+         if (k==110 || k==190) {numero(".")} //teclas de coma decimal
+         if (k==106) {operar('*')} //tecla multiplicación
+         if (k==186) {operar('*')} //tecla multiplicación
+         if (k==107) {operar('+')} //tecla suma
+         if (k==187) {operar('+')}
+         if (k==109) {operar('-')} //tecla resta
+         if (k==189) {operar('-')} //tecla resta
+         if (k==111) {operar('/')} //tecla división
+         if (k==32 || k==13) {igualar()} //Tecla igual: intro o barra espaciadora
+         if (k==46) {borradoTotal()} //Tecla borrado total: "supr"
+         if (k==8) {retro()} //Retroceso en escritura : tecla retroceso.
          }
