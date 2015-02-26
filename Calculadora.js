79bb03a7dc707a2e6e377b394732043af665c17c
mario <masan0914@yahoo.es>
2015-02-26 11:02:30 -0500

    modificacion en los archivos

--- Calculadora.js
+++ Calculadora.js
@@ -1,63 +1,17 @@
-window.onload = function(){ //Acciones tras cargar la página
-pantalla=document.getElementById("textoPantalla"); //elemento pantalla de salida
-pantalla2=document.getElementById("textoPantalla2"); //elemento pantalla de salida
-document.onkeydown = teclado;  //función teclado disponible
+function concatenar (ope){ 
+
+   {
+      pantalla=document.getElementById("textoPantalla").innerHTML = document.getElementById ("textoPantalla").innerHTML+ope; 
+   }
 }
 
-x="0"; //guardar número en pantalla
-xi=1; //iniciar número en pantalla: 1=si; 0=no;
-coma=0; //estado coma decimal 0=no, 1=si;
-ni=0; //número oculto o en espera.
-op="no"; //operación en curso; "no" =  sin operación.
+document.onkeydown = teclado;  //función teclado disponible
 
-function numero(xx) { //recoge el número pulsado en el argumento.
-         if (x=="0" || xi==1  ) { // inicializar un número, 
-            pantalla.innerHTML=xx; //mostrar en pantalla
-            x=xx; //guardar número
-            if (xx==".") { //si escribimos una coma al principio del número
-               pantalla.innerHTML="0."; //escribimos 0.
-               x=xx; //guardar número
-               coma=1; //cambiar estado de la coma
-               }
-           }
-           else { //continuar escribiendo un número
-               if (xx=="." && coma==0) { //si escribimos una coma decimal pòr primera vez
-                   pantalla.innerHTML+=xx;
-                   x+=xx;
-                   coma=1; //cambiar el estado de la coma  
-               }
-              //si intentamos escribir una segunda coma decimal no realiza ninguna acción.
-               else if (xx=="." && coma==1) {} 
-               //Resto de casos: escribir un número del 0 al 9:    
-               else {
-                   pantalla.innerHTML+=xx;
-                   x+=xx
-               }
-            }
-            xi=0 //el número está iniciado y podemos ampliarlo.
-         }
 
-function operar(s) {
-         
-         igualar(); //si hay operaciones pendientes se realizan primero
-         ni=x //ponemos el 1º número en "numero en espera" para poder escribir el segundo.
-         pantalla2.innerHTML=s; //MOSTRAR en pantalla la operacion que se realiza
-         op=s; //guardamos tipo de operación.
-         xi=1; //inicializar pantalla.
-         }  
 
-function igualar() {
-         if (op=="no") { //no hay ninguna operación pendiente.
-            pantalla.innerHTML=x;   //mostramos el mismo número   
-            }
-         else { //con operación pendiente resolvemos
-            sl=ni+op+x; // escribimos la operación en una cadena
-            sol=eval(sl) //convertimos la cadena a código y resolvemos
-            pantalla.innerHTML=sol //mostramos la solución
-            x=sol; //guardamos la solución
-            op="no"; //ya no hay operaciones pendientes
-            xi=1; //se puede reiniciar la pantalla.
-            }
+function igualar() { 
+
+         document.getElementById("textoPantalla").innerHTML=eval(document.getElementById("textoPantalla").innerHTML);
             
         }
 
@@ -84,17 +38,13 @@ function retro(){ //Borrar sólo el último número escrito.
          x=x.substr(0,cifras-1) //quitar el ultimo caracter
          if (x=="") {x="0";} //si ya no quedan caracteres, pondremos el 0
          if (br==".") {coma=0;} //Si hemos quitado la coma, se permite escribirla de nuevo.
-         pantalla.innerHTML=x; //mostrar resultado en pantalla  
+         pantalla=document.getElementById("textoPantalla").innerHTML = document.getElementById ("textoPantalla").innerHTML = x; 
          
          }
 
 function borradoTotal() {
-         pantalla.innerHTML=0; //poner pantalla a 0
-         pantalla2.innerHTML=""; //poner pantalla vacia
-         x="0"; //reiniciar número en pantalla
-         coma=0; //reiniciar estado coma decimal 
-         ni=0 //indicador de número oculto a 0;
-         op="no" //borrar operación en curso.
+          pantalla=document.getElementById("textoPantalla").innerHTML = document.getElementById ("textoPantalla").innerHTML = "";
+        
          }
 
 function pi() {
@@ -154,22 +104,24 @@ function teclado (elEvento) {
          if (k>47 && k<58) { 
             p=k-48; //buscar número a mostrar.
             p=String(p) //convertir a cadena para poder añádir en pantalla.
-            numero(p); //enviar para mostrar en pantalla
+            concatenar(p); //enviar para mostrar en pantalla
             }  
          //Teclas del teclado númerico. Seguimos el mismo procedimiento que en el anterior.
          if (k>95 && k<106) {
             p=k-96;
             p=String(p);
-            numero(p);
+            concatenar(p);
             }
-         if (k==110 || k==190) {numero(".")} //teclas de coma decimal
-         if (k==106) {operar('*')} //tecla multiplicación
-         if (k==186) {operar('*')} //tecla multiplicación
-         if (k==107) {operar('+')} //tecla suma
-         if (k==187) {operar('+')}
-         if (k==109) {operar('-')} //tecla resta
-         if (k==189) {operar('-')} //tecla resta
-         if (k==111) {operar('/')} //tecla división
+
+
+         if (k==110 || k==190) {concatenar(".")} //teclas de coma decimal
+         if (k==106) {concatenar('*')} //tecla multiplicación
+         if (k==186) {concatenar('*')} //tecla multiplicación
+         if (k==107) {concatenar('+')} //tecla suma
+         if (k==187) {concatenar('+')}
+         if (k==109) {concatenar('-')} //tecla resta
+         if (k==189) {concatenar('-')} //tecla resta
+         if (k==111) {concatenar('/')} //tecla división
          if (k==32 || k==13) {igualar()} //Tecla igual: intro o barra espaciadora
          if (k==46) {borradoTotal()} //Tecla borrado total: "supr"
          if (k==8) {retro()} //Retroceso en escritura : tecla retroceso.
